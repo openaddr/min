f2b165b247830657c8a04b9c6c01b484cb0168a2
withyyy <server.yn@qq.com>
2022-09-05 19:52:27 +0800

    0.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,15 @@ import net.mamoe.mirai.utils.BotConfiguration;
 
 public class Main {
     public static void main(String[] args) {
+        //配置并登陆QQ
         Bot bot = BotFactory.INSTANCE.newBot(2166675797l, "y1475369",new BotConfiguration(){{
             fileBasedDeviceInfo();
         }});
         bot.login();
 
+        //监听事件
         bot.getEventChannel().subscribeAlways(Event.class, event -> {
+            //将Event对象转为json,此处会报错
             String jsonStr = JSONUtil.toJsonStr(event);
             System.out.println(jsonStr);
         });
